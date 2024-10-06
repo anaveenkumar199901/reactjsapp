c0b558b6b8a293c6a9fa1145a322bb001719d6f6
Deva <pkmaurya1509@gmail.com>
2024-10-06 14:22:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Test Page to deploy ReactJS on GITHUB.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://p15s18.github.io/reactjsapp/"
           target="_blank"
           rel="noopener noreferrer"
         >
